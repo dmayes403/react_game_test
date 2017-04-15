43d1e926c59c685d81ea61ffca609aa22956b38a
dmayes403 <davidrmayes@gmail.com>
2017-04-15 13:36:23 -0600

    adding header

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render(){
+    return(
+      <section className="header_container">
+        <section className="search_criteria_box">
+          <div className="logo_icon">
+            <img src="https://cdn.worldvectorlogo.com/logos/airbnb-2.svg"/>
+          </div>
+          <form className="search_form">
+            <input className="search_form_input search_1" type="text" placeholder="Anywhere"/>
+            <input className="search_form_input" type="text" placeholder="Anytime"/>
+            <input className="search_form_input search_3" type="text" placeholder="1 guest"/>
+          </form>
+        </section>
+      </section>
+    )
+  }
+}
